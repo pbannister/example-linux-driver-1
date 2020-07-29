a819bedae08ccce6e4582233bd88b3a411067e95
Preston L. Bannister <preston@bannister.us>
2020-07-29 09:57:16 -0700

    Allocation from reserved memory.

--- driver/file-operations.c
+++ driver/file-operations.c
@@ -6,6 +6,24 @@
 #include <linux/dma-iommu.h>
 #include "common.h"
 
+// Allocate large buffers from reserved memory.
+#define HACK_RESERVED_MEMORY    1
+
+#if HACK_RESERVED_MEMORY
+
+// These values should be detected rather than assumed.
+// Assumes kernel parameter: memmap=1G!4G
+#define RESERVED_MEMORY_BASE (1LL << 32)
+#define RESERVED_MEMORY_SIZE (1LL << 30)
+
+static struct reserved_memory_s {
+    char* p_reserved;
+    char* p_free;
+    u64 size_reserved;
+} reserved_memory_g;
+
+#endif
+
 enum MINOR_DEVICES {
     MINOR_CONTROL = 0,
     MINOR_REQUEST = 1,
@@ -39,7 +57,7 @@ static struct status_s {
 typedef uint32_t* uint32_p;
 
 typedef struct mmap_buffer_s {
-    size_t size_buffer;
+    unsigned size_buffer;
     uint32_p p_buffer;
     dma_addr_t dma_handle;
 } mmap_buffer_t;
@@ -52,24 +70,24 @@ static inline bool mmap_buffer_allocate(mmap_buffer_p p) {
 }
 
 static inline bool mmap_buffer_allocate_dma(mmap_buffer_p p) {
-    if (0) {
-        // Generally follow the pattern of dma_alloc_coherent().
-        struct device* p_device = 0;
-        size_t size_wanted = p->size_buffer;
-        gfp_t gfp = GFP_KERNEL;
-        unsigned long attrs = 0;
-        const struct dma_map_ops* ops = dma_ops;
-        if (!ops->alloc) {
-            return false;
-        }
-        p->p_buffer = ops->alloc(p_device, size_wanted, &p->dma_handle, gfp, attrs);
+#if HACK_RESERVED_MEMORY
+    u64 n_used = reserved_memory_g.p_free - reserved_memory_g.p_reserved;
+    u64 n_free = reserved_memory_g.size_reserved - n_used;
+    printk(LOG_INFO "reserved free: %llx\n", n_free);
+    if (p->size_buffer <= n_free) {
+        p->p_buffer = (uint32_p) reserved_memory_g.p_free;
+        reserved_memory_g.p_free += p->size_buffer;
     }
+#endif
     //return dma_alloc_from_dev_coherent(p_device, size_wanted, &p->dma_handle, &p->p_buffer);
     //p->p_buffer = dma_alloc_attrs(p_device, size_wanted, &p->dma_handle, gfp, attrs);
     return (0 != p->p_buffer);
 }
 
 static inline void mmap_buffer_free(mmap_buffer_p p) {
+#if HACK_RESERVED_MEMORY
+    p->p_buffer = 0;
+#else
     if (p->dma_handle) {
         struct device* p_device = 0;
         dma_free_coherent(p_device, p->size_buffer, p->p_buffer, p->dma_handle);
@@ -82,6 +100,7 @@ static inline void mmap_buffer_free(mmap_buffer_p p) {
         p->p_buffer = 0;
         return;
     }
+#endif
 }
 
 //
@@ -108,6 +127,7 @@ extern unsigned example_parameter_buffer_mb_get(void);
 //
 
 static int device_open_control(struct file* p_file) {
+    // Current is not thread-safe. Move after test.
     {
         // Module parameter may have changed.
         unsigned buffer_mb = example_parameter_buffer_mb_get();
@@ -128,27 +148,37 @@ static int device_open_control(struct file* p_file) {
         buffer_antenna3.dma_handle = 0;
         buffer_antenna4.dma_handle = 0;
     }
+#if HACK_RESERVED_MEMORY
     {
         // Parse kernel command line "memmap" option?
-        // Pull from mapping for /dev/pmem0?
         // Where is our DMA memory?
-        u64 pa = (1 << 30); // @1GB
-        void* vp = phys_to_virt(pa);
-        struct page* p1 = virt_to_page(vp);
-        u64 bus = virt_to_bus(vp);
-        u64 pfn = PHYS_PFN(pa);
-        struct page* p2 = pfn_to_page(pfn);
-        printk(LOG_INFO "address physical : %llx\n", pa);
-        printk(LOG_INFO "address virtual  : %px\n", vp);
-        printk(LOG_INFO "address bus      : %llx\n", bus);
-        printk(LOG_INFO "pfn              : %llx\n", pfn);
-        printk(LOG_INFO "page (via virt)  : %px\n", p1);
-        printk(LOG_INFO "page (via pfn)   : %px\n", p1);
-        {
-            uint32_t word0 = *(uint32_p) vp;
-            printk(LOG_INFO "word 0 of page   : %08x\n", word0);
+        u64 pa_reserved = RESERVED_MEMORY_BASE;
+        reserved_memory_g.p_reserved = phys_to_virt(pa_reserved);
+        reserved_memory_g.p_free = reserved_memory_g.p_reserved;
+        reserved_memory_g.size_reserved = RESERVED_MEMORY_SIZE;
+        if (1) {
+            struct page* p1 = virt_to_page(reserved_memory_g.p_reserved);
+            u64 bus = virt_to_bus(reserved_memory_g.p_reserved);
+            u64 pfn = PHYS_PFN(pa_reserved);
+            struct page* p2 = pfn_to_page(pfn);
+            printk(LOG_INFO "address physical : %llx\n", pa_reserved);
+            printk(LOG_INFO "address virtual  : %px\n", reserved_memory_g.p_reserved);
+            printk(LOG_INFO "address bus      : %llx\n", bus);
+            printk(LOG_INFO "pfn              : %llx\n", pfn);
+            printk(LOG_INFO "page (via virt)  : %px\n", p1);
+            printk(LOG_INFO "page (via pfn)   : %px\n", p2);
+            {
+                int i;
+                uint32_p p = (uint32_p) reserved_memory_g.p_reserved;
+                for (i=0; i<10; ++i) {
+                    uint32_t v = p[i];
+                    printk(LOG_INFO "word %3d of page = %08x\n", i, v);
+                    p[i]++;
+                }
+            }
         }
     }
+#endif
     // Allocate buffers.
     if (!mmap_buffer_allocate(&buffer_request)) {
         printk(LOG_ERROR "Cannot allocate mmap memory for request!\n");
@@ -164,12 +194,12 @@ static int device_open_control(struct file* p_file) {
         printk(LOG_ERROR "Cannot allocate mmap memory for antenna4!\n");
     } else {
         // All buffers allocated - success!
-        printk(LOG_INFO "Size of request  buffer : %ld\n", buffer_request.size_buffer);
-        printk(LOG_INFO "Size of response buffer : %ld\n", buffer_response.size_buffer);
-        printk(LOG_INFO "Size of antenna1 buffer : %ld\n", buffer_antenna1.size_buffer);
-        printk(LOG_INFO "Size of antenna2 buffer : %ld\n", buffer_antenna2.size_buffer);
-        printk(LOG_INFO "Size of antenna3 buffer : %ld\n", buffer_antenna3.size_buffer);
-        printk(LOG_INFO "Size of antenna4 buffer : %ld\n", buffer_antenna4.size_buffer);
+        printk(LOG_INFO "Size of request  buffer : %u\n", buffer_request.size_buffer);
+        printk(LOG_INFO "Size of response buffer : %u\n", buffer_response.size_buffer);
+        printk(LOG_INFO "Size of antenna1 buffer : %u\n", buffer_antenna1.size_buffer);
+        printk(LOG_INFO "Size of antenna2 buffer : %u\n", buffer_antenna2.size_buffer);
+        printk(LOG_INFO "Size of antenna3 buffer : %u\n", buffer_antenna3.size_buffer);
+        printk(LOG_INFO "Size of antenna4 buffer : %u\n", buffer_antenna4.size_buffer);
         return 0;
     }
     mmap_buffer_free(&buffer_request);
